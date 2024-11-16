9ddca726e0e5ffb4b7c079f61a805992afc927fb
Noskov Alexander <miluykovalexandr@gmail.com>
2024-11-16 18:28:34 +0500

    Реализация тестовой версии REST API сервера для модуля "Развитие сотрудника" в системе "Компетентум"

--- models/competency.go
+++ models/competency.go
@@ -6,6 +6,6 @@ type Competency struct {
 	CompetencyID int       `db:"competency_id"`
 	Name         string    `db:"name"`
 	Description  string    `db:"description"`
-	ParentID     *int      `json:"parent_id"`
+	ParentID     *int      `json:"parent_id" db:"parent_id"`
 	CreateDate   time.Time `db:"create_date"`
 }
